7b590ab9c1c1a4d1c9aa05df58a81b16c6f6c3eb
İsa Diliballı <isadiliballi@outlook.com>
2019-06-06 14:59:18 +0300

    Kutulara tıklama ve verritabanındaki verileri kutulara aktarma işlemi eklendi.

---
+++ Kelime/iLetter-master/Kelime/GameViewController.swift
@@ -0,0 +1,71 @@
+//
+//  GameViewController.swift
+//  Kelime
+//
+//  Created by İsa Diliballı on 7.04.2019.
+//  Copyright © 2019 İsa Diliballı. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+import Firebase
+import FirebaseDatabase
+
+
+class GameViewController: UIViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        
+     
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+            
+            
+        }
+    
+      
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
